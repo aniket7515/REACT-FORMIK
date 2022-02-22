feefb8c1428fd675edf57e1c6a572281620c23fa
Aniket <aniketdhokane2071@gmail.com>
2022-02-22 22:59:58 +0530

    Displaying error messages

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -36,18 +36,35 @@ function YoutubeForm() {
             return errors
         }
     })
-    // console.log('Form Values',formik.values);
+    console.log('Form Values',formik.values);
   return (
     <div>
         <form action="" onSubmit={formik.handleSubmit}>
+            <div className="form-control">
             <label htmlFor="name">Name</label>
             <input type="text" id="name" name='name' onChange={formik.handleChange} value={formik.values.name}/>
+            {formik.errors.name ? <div className="error">{formik.errors.name}</div>:null}
 
+
+            </div>
+
+            <div className="form-control">
             <label htmlFor="email">E-mail</label>
             <input type="email" id='email' name='email'onChange={formik.handleChange} value={formik.values.email} /> 
+            {formik.errors.email ? <div className="error">{formik.errors.email}</div>:null}
 
+
+            </div>
+
+            <div className="form-control">
             <label htmlFor="channel" >Channel</label>
             <input type="text" id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel}/>
+            {formik.errors.channel ? <div className="error">{formik.errors.channel}</div>:null}
+
+            </div>
+            
+           
+            
 
             <button type="submit">Submit</button>
         </form>
