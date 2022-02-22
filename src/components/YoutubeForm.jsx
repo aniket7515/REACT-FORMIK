bb9cdea031d2aa4eb96e5e2a2e7773c202e70f10
Aniket <aniketdhokane2071@gmail.com>
2022-02-22 23:06:57 +0530

    Visited Fields

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -37,12 +37,13 @@ function YoutubeForm() {
         }
     })
     console.log('Form Values',formik.values);
+    console.log('Visited Fields',formik.touched);
   return (
     <div>
         <form action="" onSubmit={formik.handleSubmit}>
             <div className="form-control">
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" name='name' onChange={formik.handleChange} value={formik.values.name}/>
+            <input type="text" id="name" name='name' onChange={formik.handleChange} value={formik.values.name} onBlur={formik.handleBlur}/>
             {formik.errors.name ? <div className="error">{formik.errors.name}</div>:null}
 
 
@@ -50,7 +51,7 @@ function YoutubeForm() {
 
             <div className="form-control">
             <label htmlFor="email">E-mail</label>
-            <input type="email" id='email' name='email'onChange={formik.handleChange} value={formik.values.email} /> 
+            <input type="email" id='email' name='email'onChange={formik.handleChange} value={formik.values.email} onBlur={formik.handleBlur} /> 
             {formik.errors.email ? <div className="error">{formik.errors.email}</div>:null}
 
 
@@ -58,7 +59,7 @@ function YoutubeForm() {
 
             <div className="form-control">
             <label htmlFor="channel" >Channel</label>
-            <input type="text" id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel}/>
+            <input type="text" id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel} onBlur={formik.handleBlur}/>
             {formik.errors.channel ? <div className="error">{formik.errors.channel}</div>:null}
 
             </div>
